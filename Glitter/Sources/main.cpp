330ffae64870293c7598b6621d9362f4dc3c2697
Naksh Rathore <naksh.rathore786@gmail.com>
2026-02-16 11:43:43 -0600

    Remove redundant manual shader linking for triangle demo and soley rely on the shader class provided

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -48,33 +48,6 @@ int main() {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
   glEnableVertexAttribArray(0);
 
-  const char* vertexSrc = R"(
-  #version 400 core
-  layout(location = 0) in vec3 aPos;
-  void main() { gl_Position = vec4(aPos, 1.0); }
-  )";
-
-  const char* fragmentSrc = R"(
-  #version 400 core
-  out vec4 FragColor;
-  void main() { FragColor = vec4(1.0, 0.5, 0.2, 1.0); }
-  )";
-
-  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vs, 1, &vertexSrc, nullptr);
-  glCompileShader(vs);
-
-  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fs, 1, &fragmentSrc, nullptr);
-  glCompileShader(fs);
-
-  GLuint shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vs);
-  glAttachShader(shaderProgram, fs);
-  glLinkProgram(shaderProgram);
-  glDeleteShader(vs);
-  glDeleteShader(fs);
-
   Shader shader("Glitter/Shaders");
 
   // Rendering Loop
