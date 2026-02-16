fd208f1b9c8e265e6572434ce128ac558d98accb
Naksh Rathore <naksh.rathore786@gmail.com>
2026-02-16 00:13:12 -0600

    Redo boilerplate to have a triangle rendered

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,45 +1,97 @@
 // System Headers
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
 
-    // Load GLFW and Create a Window
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    auto mWindow = glfwCreateWindow(800, 800, "OpenGL", nullptr, nullptr);
+  // Load GLFW and Create a Window
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  auto mWindow = glfwCreateWindow(800, 800, "OpenGL", nullptr, nullptr);
 
-    // Check for Valid Context
-    if (mWindow == nullptr) {
-        fprintf(stderr, "Failed to Create OpenGL Context");
-        return EXIT_FAILURE;
-    }
+  // Check for Valid Context
+  if (mWindow == nullptr) {
+    fprintf(stderr, "Failed to Create OpenGL Context");
+    return EXIT_FAILURE;
+  }
 
-    // Create Context and Load OpenGL Functions
-    glfwMakeContextCurrent(mWindow);
-    gladLoadGL();
-    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+  // Create Context and Load OpenGL Functions
+  glfwMakeContextCurrent(mWindow);
+  gladLoadGL();
+  fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
-    // Rendering Loop
-    while (glfwWindowShouldClose(mWindow) == false) {
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(mWindow, true);
+  glViewport(0, 0, 800, 800);
 
-        // Background Fill Color
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
 
-        // Flip Buffers and Draw
-        glfwSwapBuffers(mWindow);
-        glfwPollEvents();
-    }   glfwTerminate();
-    return EXIT_SUCCESS;
+  GLuint VBO, VAO;
+
+  glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &VAO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+  glEnableVertexAttribArray(0);
+
+  const char* vertexSrc = R"(
+  #version 400 core
+  layout(location = 0) in vec3 aPos;
+  void main() { gl_Position = vec4(aPos, 1.0); }
+  )";
+
+  const char* fragmentSrc = R"(
+  #version 400 core
+  out vec4 FragColor;
+  void main() { FragColor = vec4(1.0, 0.5, 0.2, 1.0); }
+  )";
+
+  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vertexSrc, nullptr);
+  glCompileShader(vs);
+
+  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &fragmentSrc, nullptr);
+  glCompileShader(fs);
+
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vs);
+  glAttachShader(shaderProgram, fs);
+  glLinkProgram(shaderProgram);
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+
+  // Rendering Loop
+  while (glfwWindowShouldClose(mWindow) == false) {
+    if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(mWindow, true);
+
+    // Background Fill Color
+    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // Flip Buffers and Draw
+    glfwSwapBuffers(mWindow);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return EXIT_SUCCESS;
 }
