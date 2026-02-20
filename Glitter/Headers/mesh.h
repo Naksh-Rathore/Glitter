8aa14e8b324755ceaec15979c64d12e8c9d17062
Naksh Rathore <naksh.rathore786@gmail.com>
2026-02-19 19:26:21 -0600

    Add a vertex class to make mesh class simpler

--- Glitter/Headers/mesh.h
+++ Glitter/Headers/mesh.h
@@ -1,25 +1,32 @@
 #pragma once
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 #include <vector>
 
 #include "texture.h"
 #include "shader.h"
 
+struct Vertex {
+    glm::vec3 m_pos;
+    glm::vec3 m_normal;
+    glm::vec2 m_tex;
+};
+
 // A renderable mesh (without shader) 
 // Assumes the mesh being rendered is phong lit (diffuse and specular map)
 // Best used for model loading
 class Mesh {
     public:
-        std::vector<float> m_vertices;
+        std::vector<Vertex> m_vertices;
         std::vector<unsigned int> m_indices;
         std::vector<BasicTexture> m_textures;
         
         Mesh (const Mesh&) = delete;
         Mesh& operator=(const Mesh&) = delete;
 
-        Mesh(const std::vector<float>& vertices, const std::vector<unsigned int>& indices = {}, const std::vector<BasicTexture>& textures = {});
+        Mesh(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices = {}, const std::vector<BasicTexture>& textures = {});
 
         void draw(Shader& shader);
         void uploadMesh();
