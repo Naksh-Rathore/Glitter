e73450799ee5d89e0c6b0d3c561339d6c648b70f
Naksh Rathore <naksh.rathore786@gmail.com>
2026-02-19 19:26:21 -0600

    Add a vertex class to make mesh class simpler

--- Glitter/Sources/mesh.cpp
+++ Glitter/Sources/mesh.cpp
@@ -1,5 +1,6 @@
 #include "mesh.h"
 
+#include <cstddef>
 #include <glad/glad.h>
 
 #include <string>
@@ -8,7 +9,7 @@
 #include "texture.h"
 #include "shader.h"
 
-Mesh::Mesh(const std::vector<float>& vertices, const std::vector<unsigned int>& indices,  const std::vector<BasicTexture>& textures)
+Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices,  const std::vector<BasicTexture>& textures)
         : m_vertices(vertices)
         , m_indices(indices)
         , m_textures(textures)
@@ -21,17 +22,17 @@ void Mesh::uploadMesh() {
     glBindVertexArray(m_VAO);
     glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 
-    glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(float), m_vertices.data(), GL_STATIC_DRAW);  
+    glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(Vertex), &m_vertices[0], GL_STATIC_DRAW);  
 
     // vertex positions
     glEnableVertexAttribArray(0);	
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
     // vertex normals
     glEnableVertexAttribArray(1);	
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, m_normal));
     // vertex texture coords
     glEnableVertexAttribArray(2);	
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, m_tex));
 
     if (m_indices.empty())
         return;
