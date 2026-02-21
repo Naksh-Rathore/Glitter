be57b26148d0cb3fab12f95aaba6f603b17dc810
Naksh Rathore <naksh.rathore786@gmail.com>
2026-02-20 21:15:09 -0600

    add init function to clean up main.cpp

---
+++ Glitter/Sources/init.cpp
@@ -0,0 +1,51 @@
+#include "init.h"
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <stb_image.h>
+
+namespace Init {
+    void framebuffer_size_callback([[maybe_unused]] GLFWwindow *window, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    GLFWwindow* init() {
+        if (!glfwInit()) {
+            std::cerr << "GLFW failed to initialize!\n";
+            return nullptr;
+        }
+        
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        GLFWwindow *window { glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "3D Maze Runner", nullptr, nullptr) };
+
+        if (window == nullptr) {
+            std::cerr << "Failed to create GLFW window\n";
+            glfwTerminate();
+            return nullptr;
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            std::cerr << "Failed to initialize GLAD\n";
+            
+            glfwTerminate();
+            return nullptr;
+        }
+
+        glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        
+        stbi_set_flip_vertically_on_load(true);
+
+        std::cout << "OpenGL " << glGetString(GL_VERSION) << "\n";
+
+        return window;
+    }
+}
