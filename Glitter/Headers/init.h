d67a36918e811cdb1ed4b5119010864d96af8243
Naksh Rathore <naksh.rathore786@gmail.com>
2026-03-06 22:08:23 -0600

    Use a debug context for ease in debug use

--- Glitter/Headers/init.h
+++ Glitter/Headers/init.h
@@ -9,6 +9,9 @@
 namespace Init {
     void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
+    void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity, 
+                            GLsizei length, const char *message, const void *userParam);
+
     GLFWwindow* init();
 };
 
--- Glitter/Sources/init.cpp
+++ Glitter/Sources/init.cpp
@@ -15,6 +15,53 @@ namespace Init {
         glViewport(0, 0, frameBufferWidth, frameBufferHeight);
     }
 
+    void APIENTRY glDebugOutput(GLenum source, 
+                            GLenum type, 
+                            unsigned int id, 
+                            GLenum severity, 
+                            GLsizei length, 
+                            const char *message, 
+                            const void *userParam)
+    {
+        // ignore non-significant error/warning codes
+        if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return; 
+
+        std::cout << "---------------" << std::endl;
+        std::cout << "Debug message (" << id << "): " <<  message << std::endl;
+
+        switch (source)
+        {
+            case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+            case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+            case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+            case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+            case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+            case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+        } std::cout << std::endl;
+
+        switch (type)
+        {
+            case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break; 
+            case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+            case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+            case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+            case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+            case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+            case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+        } std::cout << std::endl;
+        
+        switch (severity)
+        {
+            case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+            case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+            case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+            case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+        } std::cout << std::endl;
+        std::cout << std::endl;
+    }
+
     GLFWwindow* init() {
         if (!glfwInit()) {
             std::cerr << "GLFW failed to initialize!\n";
@@ -24,6 +71,8 @@ namespace Init {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);  
 
         GLFWwindow *window { glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGL", nullptr, nullptr) };
 
@@ -42,6 +91,17 @@ namespace Init {
             glfwTerminate();
             return nullptr;
         }
+
+        int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+
+        if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+        {
+            glEnable(GL_DEBUG_OUTPUT);
+            glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); 
+            glDebugMessageCallback(glDebugOutput, nullptr);
+            glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+        }
+
 
         int frameBufferWidth, frameBufferHeight;
 
