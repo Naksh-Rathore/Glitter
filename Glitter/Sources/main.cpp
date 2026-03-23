2d54a2e261c8126e09d2b89b4b18c04086470efa
Naksh Rathore <naksh.rathore786@gmail.com>
2026-03-22 19:30:24 -0500

    Use implemented uploadMeshBuffers function for the skybox cube

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -84,18 +84,11 @@ int main(int argc, char **argv) {
         -1.0f, -1.0f, -1.0f,  -1.0f, -1.0f,  1.0f,   1.0f, -1.0f, -1.0f,   1.0f, -1.0f, -1.0f,  -1.0f, -1.0f,  1.0f,   1.0f, -1.0f,  1.0f
     };
 
-    unsigned int skyboxVAO, skyboxVBO;
+    GLuint skyboxVAO, skyboxVBO, skyboxEBO;
 
-    glGenVertexArrays(1, &skyboxVAO);
-    glGenBuffers(1, &skyboxVBO);
+    skyboxEBO = 0;
 
-    glBindVertexArray(skyboxVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
-    
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    uploadMeshBuffers(skyboxVertices, sizeof(skyboxVertices), skyboxVAO, skyboxVBO, skyboxEBO);
 
     Shader shader("Glitter/Assets/shaders");
 
