d64f827bf01b7355db1054b05b0a9b9d20500458
Naksh Rathore <naksh.rathore786@gmail.com>
2026-03-26 20:33:59 -0500

    Fix typo

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -101,7 +101,7 @@ int main(int argc, char **argv) {
 
     shader.setVec3("light.position", glm::vec3(0.0f, 1.0f, 0.0f));
 
-    shader.setVec3("light.ambient", glm::vec3(0.22f));
+    shader.setVec3("light.ambient", glm::vec3(0.2f));
     shader.setVec3("light.diffuse", glm::vec3(0.5f));
     shader.setVec3("light.specular", glm::vec3(1.0f));
 
