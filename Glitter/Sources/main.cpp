1a4dec3efb274b6f96d3039bff4921ffdd347721
Naksh Rathore <naksh.rathore786@gmail.com>
2026-05-27 13:48:13 -0500

    Have sponza model

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char **argv) {
 
     glEnable(GL_DEPTH_TEST);
 
-    Model model("Glitter/Assets/dining-room/dining-room.obj");
+    Model model("Glitter/Assets/sponza-palace/sponza-palace.obj");
 
     Shader shader("Glitter/Assets/shaders");
 
